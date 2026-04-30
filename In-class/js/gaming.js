71ff0501f7259389606f76d8cb08e7111bb6b325
Jake.C <jakecharnley49@hotmail.com>
2026-04-30 12:57:16 +0100

    adding js to every page

---
+++ In-class/js/gaming.js
@@ -0,0 +1,5 @@
+const url = 'https://content.guardianapis.com/search?q=gaming&api-key=';
+const apiKey = '87eb1bd2-be60-4cb9-81c9-4d51620f6dcf';
+fetch(url + apiKey)
+.then((response) => response.json())
+.then((data) => console.log(data));
