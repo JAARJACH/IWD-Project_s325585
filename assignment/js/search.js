049c6fc6934d484b12f99870c3e66a29076e7072
Jake.C <jakecharnley49@hotmail.com>
2026-05-04 22:46:54 +0100

    mkaing the shows show up on the grid system as design

--- assignment/js/search.js
+++ assignment/js/search.js
@@ -1,26 +1,28 @@
-const searchNews = (value) => {
+const searchShows = (event) => {
     event.preventDefault();
+    console.log("hi");
     const keyword = document.querySelector('#keywords').value;
-    const url = 'https://api.tvmaze.com/singlesearch/shows?q=' + keyword;
+    const url = 'https://api.tvmaze.com/search/shows?q=' + keyword;
     const resultList = document.querySelector('#results');
     resultList.innerHTML = '';
-
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
-    data.response.results.forEach(function (value) {
-        console.log(value);
-        const articleElement = `
-        <div>
+        console.log(data);
+        data.forEach((item) => {
+            const show = item.show;
+            const articleElement = `
             <div class="bg-white rounded shadow border-2 p-4 mb-4">
-                <h5 class="text-lg font-semibold mb-2">${value.webTitle}</h5>
-                <p class="text-gray-600 mb-3">${value.sectionName}</p>
-                <a target="_blank" href="${value.webUrl}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                    View Article
+                <h5 class="text-lg font-semibold mb-2 break-words">${show.name}</h5>
+                <p class="text-gray-600 mb-3 break-words">${show.genres}</p>
+                <a target="_blank" href="${show.officialSite || '#'}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                    Visit Show
                 </a>
-            </div>
-        </div>`;
-        resultList.insertAdjacentHTML('beforeend', articleElement);
-    });
+            </div>`;
+            resultList.insertAdjacentHTML('beforeend', articleElement);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
     });
 }
