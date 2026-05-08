f18706634c3469e1f6981bd990141d2149bdeeb1
Jake.C <jakecharnley49@hotmail.com>
2026-05-08 16:15:29 +0100

    changing the java script to forget about the local data beacuase its to complicated. and creating a home page shows to be displayed

--- assignment/js/search.js
+++ assignment/js/search.js
@@ -34,50 +34,38 @@ const searchShows = (event) => {
     });
 }
 
-const displayShows = (data, searchValue) => {
-
+const homePageShows = () =>{
+    event.preventDefault();
     resultList.innerHTML = '';
-    document.querySelector('#keywords').value = searchValue;
-
-    data.forEach((item) => {
-
-        const show = item.show;
-
-        const ShowElement = `
-        <div class="bg-white rounded shadow border-2 p-4 mb-4">
+    for(let i = 1; i < 5; i++){
+        const url = 'https://api.tvmaze.com/shows/' + i;
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            localStorage.setItem("shows", JSON.stringify(data));
+            const ShowElement = `
+            <div class="bg-white rounded shadow border-2 p-4 mb-4">
                 <img 
-                    src="${show.image?.medium}" 
-                    alt="${show.name}"
+                    src="${data.image?.medium}" 
+                    alt="${data.name}"
                     class="w-full rounded mb-4"
                 >
-                <h5 class="text-lg font-semibold mb-2 break-words">${show.name}</h5>
-                <p class="text-gray-600 mb-3 break-words">${show.genres}</p>
-                <a target="_blank" href="${show.officialSite || '#'}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                <h5 class="text-lg font-semibold mb-2 break-words">${data.name}</h5>
+                <p class="text-gray-600 mb-3 break-words">${data.genres}</p>
+                <a target="_blank" href="${data.officialSite || '#'}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
                     Visit Show
                 </a>
             </div>`;
-
-        resultList.insertAdjacentHTML('beforeend', ShowElement);
-    });
-};
+            resultList.insertAdjacentHTML('beforeend', ShowElement);
+            
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+}
 
 window.addEventListener("load", () => {
-    
-    if (performance.getEntriesByType("navigation")[0].type === "reload"){
-        const savedShows = localStorage.getItem("shows");
-        const savedSearchValue = localStorage.getItem("search value");
-
-        if (savedShows) {
-
-            const showData = JSON.parse(savedShows);
-            const searchValueData = JSON.parse(savedSearchValue);
-
-            displayShows(showData, searchValueData);
-        }
-    }
-    else{
-        localStorage.clear();
-    }
-        
-    
+    homePageShows();    
 });
