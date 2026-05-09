fdde4f0ce2fe6b936c245f3f25fa59db087c402f
Jake.C <jakecharnley49@hotmail.com>
2026-05-09 01:19:44 +0100

    fixing the topp 100 page

--- assignment/js/index.js
+++ assignment/js/index.js
@@ -25,22 +25,22 @@ Top100Shows.addEventListener('click', () => {
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
-        data.forEach((show) => {
-            const sortedShows = shows
-                .filter(show => show.weight !== null)
+        const sortedShows = data
+                .filter(show => show.weight != null)
                 .sort((a, b) => b.weight - a.weight)
                 .slice(0, 100);
-            if(sortedShow.officialSite != null){
+            sortedShows.forEach((show) => {
+            if(show.officialSite != null){
                 const ShowElement = `
                 <div class="bg-white rounded shadow border-2 p-4 mb-4">
                     <img 
-                        src="${sortedShow.image?.medium}" 
-                        alt="${sortedShow.name}"
+                        src="${show.image?.medium}" 
+                        alt="${show.name}"
                         class="w-full rounded mb-4"
                     >
-                    <h5 class="text-lg font-semibold mb-2 break-words">${sortedShow.name}</h5>
-                    <p class="text-gray-600 mb-3 break-words">${sortedShow.genres}</p>
-                    <a target="_blank" href="${sortedShow.officialSite || ''}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                    <h5 class="text-lg font-semibold mb-2 break-words">${show.name}</h5>
+                    <p class="text-gray-600 mb-3 break-words">${show.genres}</p>
+                    <a target="_blank" href="${show.officialSite || ''}" class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
                         Visit Official Site
                     </a>
                 </div>`;
@@ -50,12 +50,12 @@ Top100Shows.addEventListener('click', () => {
                 const ShowElement = `
                 <div class="bg-white rounded shadow border-2 p-4 mb-4">
                     <img 
-                        src="${sortedShow.image?.medium}" 
-                        alt="${sortedShow.name}"
+                        src="${show.image?.medium}" 
+                        alt="${show.name}"
                         class="w-full rounded mb-4"
                     >
-                    <h5 class="text-lg font-semibold mb-2 break-words">${sortedShow.name}</h5>
-                    <p class="text-gray-600 mb-3 break-words">${sortedShow.genres}</p>
+                    <h5 class="text-lg font-semibold mb-2 break-words">${show.name}</h5>
+                    <p class="text-gray-600 mb-3 break-words">${show.genres}</p>
                     <p class="inline-block text-gray-600 px-4 py-2 rounded">
                         Official Site Not Available
                     </p>
